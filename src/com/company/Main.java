462d61ee8f4ece9d279b73fa331e81b816f430c9
Dylan Black <black626@gmail.com>
2018-05-02 21:15:08 -0500

    Factory Method working now. Trimmed code and working properly.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,20 @@
 package com.company;
 import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+/*
+This program takes a user-inputted amount of lines and makes a 'Mario-style' pyramid in response.
+It can output to both the console, or an output file, determined by the user.
+ */
 
 public class Main {
 
     public static void main(String[] args)
     {
 	    int number; //number of lines to use
-	    boolean filePrint; //determines if we print to output file or not
         Scanner reader = new Scanner(System.in); //Initialize scanner for input
+        String fileName = "output.txt"; //output text file
 
 	    System.out.println("Welcome to the Mario program!");
         System.out.println("How many lines do you want the pyramid to be? (0-23)");
@@ -21,10 +28,35 @@ public class Main {
 
         System.out.println("Do you want to print the output to a separate file? (Y/N)");
 	    char answer = reader.next().charAt(0); //This will save answer as either 'y' or 'n', regardless if user answered
+        //Check if user inputted correct answer (upper or lower case 'y' or 'n' will work)
+        while (Character.toLowerCase(answer) != 'y' && Character.toLowerCase(answer) != 'n')
+        {
+            System.out.println("Do you want to print the output to a separate file? (Y/N)");
+            answer = reader.next().charAt(0);
+        }
         reader.close(); //Close scanner to save resources
 
         //Send answers to our 'pyramid factory' for output.
         String result = new Pyramid().makePyramid(number);
-        System.out.println(result);
+
+        //If we want to print to file:
+        if (Character.toLowerCase(answer) == 'y')
+        {
+            try
+            {
+                PrintWriter outputStream = new PrintWriter(fileName);
+                outputStream.print(result);
+                outputStream.close();
+            }
+            catch (FileNotFoundException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        //Else, print to console.
+        else
+        {
+            System.out.println(result);
+        }
     }
 }
--- src/com/company/Pyramid.java
+++ src/com/company/Pyramid.java
@@ -1,14 +1,18 @@
 package com.company;
 
+/*
+This class is an attempt at trying the 'Factory Design Pattern'.
+It takes an integer sent to it and makes a pyramid out of it, aligned to the right.
+The output is in the form of a String.
+ */
+
 public class Pyramid
 {
-    private char[][] lines; //Array of 'chars' to represent each line of the pyramid.
-
     public String makePyramid(int number)
     {
         int variable = 20+number; //This number is what we'll base the spacing on during the loop.
         int upperBound = variable + 2;
-        lines = new char[number][upperBound]; //Instantiate [rows][columns] of 2D char array.
+        StringBuilder result = new StringBuilder(); //Efficient to append each char to a StringBuilder.
 
         //Creates a number of lines for 'drawing' as defined by 'n'.
         for (int i = 0; i < number; i++)
@@ -16,35 +20,29 @@ public class Pyramid
             //Saves our spaces.
             for (int j = 0; j < variable; j++)
             {
-                lines[i][j] = (' ');
+                result.append(' ');
             }
-            //Saves our #'s. (Make sure to put them at the end of the spaces, hence why we do 'k+variable')
+            //Saves our #'s.
             for (int k = 0; k < (upperBound-variable); k++)
             {
-                lines[i][k+variable] = ('#');
+                result.append('#');
             }
             variable--; //We negatively increment this so that our spaces will go down as our amount of #'s go up.
+            //Sets the pyramid to a new line:
+            if (i < (number-1)) //(With the exception of the last line)
+            {
+                result.append('\n');
+            }
         }
-        return lines.toString();
+
+        return result.toString();
+        //Stringbuilder already has a .toString override function to suit our needs.
     }
 
 
-    @Override
+    /*@Override
     public String toString()
     {
 
-        //StringBuilder result = new StringBuilder(); //More efficient to append each char to a StringBuilder.
-        String result = "";
-        for (int i = 0; i < lines.length; i++) //Go through each row
-        {
-            for (int j = 0; j <lines[0].length; j++) //Go through each column
-                //Note: this only works because we know that our 2D array has a row @ [0], otherwise outOfBounds exception.
-            {
-                //result.append(lines[i][j]);
-                result += lines[i][j];
-            }
-            result += "\n";
-        }
-        return (result);
-    }
+    }*/
 }
